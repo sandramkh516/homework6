515666dae12beaa081de26499491026f03163d05
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2023-04-15 12:02:37 +0430

    add link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="App">
       <Weather defaultCity="Isfahan" />
+      <footer>
+        this project is coded by sara mokhtari and is{" "}
+        <a href="https://github.com/sandramkh516/homework6">open-source</a>
+      </footer>
     </div>
   );
 }
